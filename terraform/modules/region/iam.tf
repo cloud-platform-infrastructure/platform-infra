3485e1f5d0866108fb62661af27f548d849ad9e4
Guilherme Kishimoto <gui.kishimoto@gmail.com>
2026-01-05 09:47:32 -0300

    add permission to app-frontend

--- terraform/modules/region/iam.tf
+++ terraform/modules/region/iam.tf
@@ -51,7 +51,8 @@ data "aws_iam_policy_document" "kubectl_admin_trust" {
       test     = "StringLike"
       variable = "token.actions.githubusercontent.com:sub"
       values = ["repo:cloud-platform-infrastructure/platform-infra:ref:refs/heads/main",
-      "repo:cloud-platform-infrastructure/deploy-workflows:ref:refs/heads/main"]
+      "repo:cloud-platform-infrastructure/deploy-workflows:ref:refs/heads/main", 
+      "repo:cloud-platform-infrastructure/app-frontend:ref:refs/heads/main"]
     }
   }
 }
